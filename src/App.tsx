7cb15afc21a73041d7fc845ec0a6728d7d393984
unoursmarin <raphaelchouchane@gmail.com>
2023-09-20 01:16:10 +0200

    Consume api and interface userData

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+interface UserData {
+  name: string;
+  website: string;
+  email: string;
+  phone: string;
+}
+
 function App() {
+  const [userData, setUserData] = useState<UserData>({
+    name: "",
+    website: "",
+    email: "",
+    phone: "",
+  });
+  const url = "https://jsonplaceholder.typicode.com/users/1";
+
+  useEffect(() => {
+    // Fetch user data from the URL
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setUserData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching user data:', error);
+        });
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+        <h2>User Data</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <strong>Name: </strong>
+          {userData.name || "(Need to populate name here)"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <strong>Website: </strong>
+          {userData.website || "(Need to populate website here)"}
+        </p>
+        <p>
+          <strong>Email: </strong>
+          {userData.email || "(Need to populate email here)"}
+        </p>
+        <p>
+          <strong>Phone: </strong>
+          {userData.phone || "(Need to populate phone here)"}
+        </p>
+      </div>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
